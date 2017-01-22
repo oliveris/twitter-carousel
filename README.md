12119085f8447742d6354fe4574a4ca80f44e3fa
Sam Oliveri <sam.oliveri@cigroup.co.uk>
2017-01-22 10:17:19 -0800

    changed into a plugin directory layout

--- README.md
+++ README.md
@@ -7,3 +7,17 @@ http://www.twitter-feed.your-development.co.uk/test/</p>
 <p>Will pull through a Twitter users 10 latest posts using Twitter API 1.1.<br>
 This version pulls through the data in JSON format.<br>
 JQuery then processes the data to display within a Bootstrap carousel.</p>
+
+<h3 style="margin-bottom:0px">Version 1.1</h3>
+<p>Moved the HTML, CSS and JS into it own plugin directory.<br>
+The other files listed below need to remain in the directory that you set in the 
+Twitter application:</p>
+<ul>
+<li>cacert.pem - is a bundle of CA certificates that you use to verify that the 
+server is really the correct site you're talking to (when it presents its 
+certificate in the SSL handshake).</li>
+<li>tmhOAuth.php - An OAuth library written in PHP by @themattharris available
+on GitHub</li>
+<li>tweets_json.php - makes the connection to the Twitter API using the the consumer key +
+secret and access token + secret, this also gets the post values to obtain the data</li>
+</ul>
---
+++ plugin/twitter-carousel/twitter-carousel.js
@@ -0,0 +1,68 @@
+/**
+ * Created by samoliveri on 22/01/2017.
+ */
+$(document).ready(function() {
+    //i variable declared to increment and assign a new class each time to the div
+    var i = 1;
+
+    //get the last 10 Twitter post and stores them in a JSON array
+    $.getJSON('tweets_json.php?count=10', function(data) {
+        listTweets(data);
+    });
+
+    /**
+     * Function to create the tweet div and append the tweet in the div
+     * @param data
+     */
+    function listTweets(data) {
+        $.each(data, function(index) {
+            //logs data to console
+            console.log(data[index]);
+
+            /**
+             * Creates a new tweet div
+             * if i = 1 need to set active
+             */
+            if(i == 1) {
+                $('#tweetList').append('<div class="tweet-' + i + ' item active"></div>');
+            } else {
+                $('#tweetList').append('<div class="tweet-' + i + ' item"></div>');
+            }
+
+            //append the variable data into the new tweet div
+            $('.tweet-' + i + '').append(checkURL(data[index]['text']));
+
+            //increments the value of i
+            increment();
+        });
+    }
+
+    /**
+     * Function to increment the value of i
+     * @returns {int}
+     */
+    function increment(){
+        i++;
+        return i;
+    }
+
+    /**
+     * Function to convert string value words with http and https with links
+     * @param text
+     * @returns {string}
+     * @constructor
+     */
+    function checkURL(text) {
+        var url1 = /(^|&lt;|\s)(www\..+?\..+?)(\s|&gt;|$)/g,
+            url2 = /(^|&lt;|\s)(((https?|ftp):\/\/|mailto:).+?)(\s|&gt;|$)/g;
+
+        var html = $.trim(text);
+        if (html) {
+            html = html
+                .replace(url1, '$1<a class="blue-link" target="_blank" href="http://$2">$2</a>$3')
+                .replace(url2, '$1<a class="blue-link" target="_blank" href="$2">$2</a>$5');
+        }
+        return html;
+    }
+
+});
